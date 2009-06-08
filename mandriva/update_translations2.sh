f1d6730940b2302eccd97a2e8955d7aa03f495b2
P. Christeas <p_christ@hol.gr>
2009-06-09 01:07:26 +0300

    One more script, to merge .pot files into .po ones.

---
+++ mandriva/update_translations2.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+# This script is free software, Copyright (C) P. Christeas <p_christ@hol.gr>, 2009
+
+# Use this script to update .po files at modules.
+
+Usage() {
+cat '-' <<EOF
+
+Usage:
+	$0 -h
+	$0 [-l <lang>] [<addons-path> ...]
+	
+Description:
+	Update the .po files for some language in (all) addons paths.
+	If no addons paths are specified, then "./addons" plus any found
+	at ~/openerp-server.conf are used.
+	
+Parameters:
+	-h         Display this help
+	-l <lang>  Operate only for language <lang>
+	<addons-path>  Manually specify addons path(s)
+
+EOF
+}
+
+
+OERP_CONF=~/openerp-server.conf
+OERP_DB=testdb
+
+if [ "$1" == "-h" ] ; then
+	Usage
+	exit 0
+fi
+
+USE_LANG='*'
+
+if [ "$1" == "-l" ] ; then
+	USE_LANG="$2"
+	shift 2
+fi
+
+declare -a ADDONS_PATH
+
+if [ -n "$1" ] ; then
+	echo "trying at $@"
+	ADDONS_PATH=( "$@" )
+else
+	# try to figure out the addons path
+	ADDONS_PATH=( $( [ -d ./addons/ ] && echo $(pwd)/addons/)
+		$(cat ~/openerp-server.conf | grep '^addons_path' | sed 's/^.*=//' | \
+		while read -d ',' APATH ; do 
+			echo $APATH
+		done) )
+fi
+
+for ADDON_PATH in ${ADDONS_PATH[@]} ; do pushd "$ADDON_PATH"
+
+	for DIR in * ; do
+		[ ! -d "$DIR/i18n" ] && continue
+		if [ ! -r "$DIR/i18n/$DIR.pot" ] ; then
+			echo "No useful .pot for $DIR"
+			continue
+		fi
+		
+		for POFILE in "$DIR/i18n/"$USE_LANG.po ; do
+			if [ ! -r "$POFILE" ] ; then
+				continue
+			fi
+			
+			if [ "$DIR/i18n/$DIR.pot" -nt "$POFILE" ] ; then
+				echo "Updating $POFILE"
+				msgmerge -U "$POFILE" "$DIR/i18n/$DIR.pot"
+			fi
+		done
+	done
+popd ; done
+
+#eof
