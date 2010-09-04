b074acf3ab4275c487c77791908c86da2ed2f98a
P. Christeas <p_christ@hol.gr>
2010-09-04 18:24:57 +0300

    tests/run_bqi: have some trivial options
    Usage: (strict order, so far)
        -d    Enable debugging
        -n    Don't drop db, continue with the same
        -u    Upgrade the modules, don't attempt to install them.

--- tests/run-bqi.sh
+++ tests/run-bqi.sh
@@ -4,16 +4,38 @@ cd $(dirname "$0")
 cd ..
 
 MODULES=
+RECREATE_DB="-drop-db create-db"
+
+BQI_DEBUG=
+if  [ "$1" == '-d' ] ;then
+    BQI_DEBUG=--debug
+    shift 1
+fi
+
+if [ "$1" == '-n' ] ; then
+    RECREATE_DB=
+    shift 1
+fi
+
+INST_CMD=install-module
+if  [ "$1" == '-u' ] ;then
+    INST_CMD=upgrade-module
+    shift 1
+fi
+
+
 if [ -z "$1" ] ; then
     MODULES="hr sale purchase"
 else
     MODULES="$@"
 fi
 
+
+
 ./buildbot/openerp_buildbot_slave/base_quality_interrogation.py -p 8169 -d test_bqi \
-    --server-series=pg84 --homedir=./ \
-    --debug --machine-log=stdout --addons-path=./addons --root-path=./server/bin/ \
-    -- -create-db install-module $MODULES -- check-quality $MODULES | \
+    --server-series=pg84 --homedir=./ $BQI_DEBUG \
+    --machine-log=stdout --addons-path=./addons --root-path=./server/bin/ \
+    -- $RECREATE_DB $INST_CMD $MODULES  | \
     tee test-bqi.log
 
 #eof
