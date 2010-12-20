ad410bc12913f5c90dab4bb58ca24fc73e851f79
P. Christeas <p_christ@hol.gr>
2010-12-20 10:43:38 +0200

    scripts: enhance the build_transdir.sh to recognize web-client dirs

--- mandriva/build_transdir.sh
+++ mandriva/build_transdir.sh
@@ -9,7 +9,7 @@ cat '-' <<EOF
 
 Usage:
 	$0 -h
-	$0 [-l <lang>] [<addons-path> ...]
+	$0 [-l <lang>] [-T] [<addons-path> ...]
 	
 Description:
 	Find .po files for some language in (all) addons paths. Generate
@@ -20,6 +20,7 @@ Description:
 Parameters:
 	-h         Display this help
 	-l <lang>  Operate only for language <lang>
+	-T         Skip the template dirs
 	<addons-path>  Manually specify addons path(s)
 
 EOF
@@ -28,6 +29,7 @@ EOF
 
 OERP_CONF=~/openerp-server.conf
 OERP_DB=testdb
+DO_SKIP_TEMPLATES=
 
 if [ "$1" == "-h" ] ; then
 	Usage
@@ -40,6 +42,10 @@ USE_LANG='*'
 if [ "$1" == "-l" ] ; then
 	USE_LANG="$2"
 	shift 2
+fi
+
+if [ "$1" == "-T" ] ; then
+    DO_SKIP_TEMPLATES=y
 fi
 
 declare -a ADDONS_PATH
@@ -57,26 +63,45 @@ fi
 
 # create the structure
 
-[ ! -d "./templates" ] && ( mkdir './templates' || exit $? )
+[ ! -d "./templates" ] && [ "$DO_SKIP_TEMPLATES" != "y" ] && ( mkdir './templates' || exit $? )
 
+LinkPoFile() {
+	DIR="$1"
+	POFILE="$2"
+	PREFIX="$3"
+	SUFFIX="$4"
+	if [ ! -r "$POFILE" ] ; then
+		return
+	fi
+	THE_LANG=$(basename "$POFILE" .po)
+	if [ ! -d "$TARGET_DIR/$THE_LANG" ] ; then
+		mkdir "$TARGET_DIR/$THE_LANG" || exit $?
+	fi
+	ln -sf "$(pwd)/$POFILE" "$TARGET_DIR/$THE_LANG/${PREFIX}$DIR${SUFFIX}.po"
+}
 
 for ADDON_PATH in ${ADDONS_PATH[@]} ; do pushd "$ADDON_PATH"
 	for DIR in * ; do
-		[ ! -d "$DIR/i18n" ] && continue
-		if [ -r "$DIR/i18n/$DIR.pot" ] ; then
+	    if [ -d "$DIR/i18n" ] ; then
+		if [ -r "$DIR/i18n/$DIR.pot" ] && [ "$DO_SKIP_TEMPLATES" != "y" ] ; then
 			ln -sf "$(pwd)/$DIR/i18n/$DIR.pot" "$TARGET_DIR/templates/$DIR.pot"
 		fi
 		for POFILE in "$DIR/i18n/"$USE_LANG.po ; do
-			if [ ! -r "$POFILE" ] ; then
-				continue
-			fi
-			THE_LANG=$(basename "$POFILE" .po)
-			if [ ! -d "$TARGET_DIR/$THE_LANG" ] ; then
-				mkdir "$TARGET_DIR/$THE_LANG" || exit $?
-			fi
-			ln -sf "$(pwd)/$POFILE" "$TARGET_DIR/$THE_LANG/$DIR.po"
+			LinkPoFile "$DIR" "$POFILE"
 		done
 		# confusing, isn't it?
+	    fi
+	    if [ -d "$DIR/po/messages" ] ; then
+		echo "Found $DIR/po/messages"
+		for POFILE in "$DIR/po/messages/"$USE_LANG.po ; do
+			LinkPoFile "$DIR" "$POFILE" web- -messages
+		done
+	    fi
+	    if [ -d "$DIR/po/javascript" ] ; then
+		for POFILE in "$DIR/po/javascript/"$USE_LANG.po ; do
+			LinkPoFile "$DIR" "$POFILE" web- -javascript
+		done
+	    fi
 	done
 done
 
