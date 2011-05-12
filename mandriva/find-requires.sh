c594d88bb22dd92ca61fb85f8077f9293aef0684
P. Christeas <p_christ@hol.gr>
2011-05-13 01:28:00 +0300

    mandriva/mageia: try to reduce the rpm requires

--- mandriva/find-requires.sh
+++ mandriva/find-requires.sh
@@ -5,11 +5,15 @@ TMP_STDIN=$(mktemp)
 cat '-' > $TMP_STDIN
 EXIT=0
 
-
-RPM_FIND_REQUIRES=/usr/lib/rpm/find-requires
-
 VENDOR=$(rpm --eval '%{_target_vendor}')
 
+# in server/setup.py we can magically turn off the requirements,
+# so that addons ask for them explicitly. Mageia's find-requires will
+# call setup.py, so make sure we get the mininmal requirements.
+export NO_INSTALL_REQS=1
+export NO_INSTALL_EXTRA_REQS=1
+
+RPM_FIND_REQUIRES=/usr/lib/rpm/find-requires
 if [ -n "$1" ] ; then
 	RPM_FIND_REQUIRES="$1"
 elif [ -x /usr/lib/rpm/$VENDOR/find-requires ]; then
