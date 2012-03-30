d85d8e0ceda49d0ff2551bba57b7bff0af3b945b
P. Christeas <xrg@linux.gr>
2012-03-30 18:52:13 +0300

    packaging: refactor, improve the python dependency resolver
    Refactor the identical blocks into one common module. Then, improve it
    to look for the '__init__.py' file if "rpm -q -f .." cannot find the
    module directory in the RPM db.

--- mandriva/modulize.py
+++ mandriva/modulize.py
@@ -29,16 +29,16 @@
 #
 # Convert an OpenERP module to RPM package
 
-import imp
 import sys
 import os
-import errno
 #import glob
-import subprocess
 import re
-import ConfigParser
+import subprocess
 
 from optparse import OptionParser
+
+sys.path.insert(0, '.')
+from modulize_utils import get_depends, get_module_info, get_ext_depends, get_extern_depends
 
 parser = OptionParser()
 parser.add_option("-q", "--quiet",
@@ -182,126 +182,6 @@ install -d $RPM_BUILD_ROOT/%{python_sitelib}/openerp-server/addons
 cp -ar ./* $RPM_BUILD_ROOT/%{python_sitelib}/openerp-server/addons/
 """
 
-def get_module_info(name):
-    try:
-        mpath = os.path.join(name, '__openerp__.py')
-        if not os.path.exists(mpath):
-            mpath = os.path.join(name, '__terp__.py')
-        f = open(mpath, "r")
-        data = f.read()
-        info = eval(data)
-        if 'version' in info:
-            info['version'] = rel.mainver + '.' + info['version']+rel.subver
-        f.close()
-    except IOError:
-        sys.stderr.write("Dir at %s may not be an OpenERP module.\n" % name)
-        return {}
-    except:
-        sys.stderr.write(str( sys.exc_info()))
-        return {}
-    return info
-
-def get_depends(deps,allnames):
-    ret = []
-    for dep in deps:
-        if dep == "base" : continue
-        if dep in allnames:
-            ret.append(oname+"-"+dep)
-        else:
-            ret.append('openerp-addons-'+dep)
-    ret = set(ret)
-    if not ret : return ""
-    return "Requires: %s \n" % (", ".join(ret))
-
-def get_ext_depends(deps):
-    # Write down dependencies for non-openerp python modules
-    # The format can either be a string, or a tuple, with the version required
-    ret = []
-    ret_ver = []
-    str_ret = ''
-    for dep in deps:
-        if type(dep) == type(tuple()):
-            ret_ver.append(dep[0]+' '+ dep[1] + ' ' + dep[2])
-        else:
-            ret.append(dep)
-    ret = set(ret)
-    ret_ver = set(ret_ver)
-    if ret:
-        str_ret += "Requires: %s \n" % (", ".join(ret))
-    if ret_ver:
-        str_ret += "".join(map(lambda a: "Requires: %s\n" % a, ret_ver))
-    return str_ret
-
-def which(dep, paths=[]):
-    """ Locate `dep` among `paths`
-
-        Note: paths will be preserved accross calls
-    """
-
-    if not paths:
-        paths = [ p for p in os.environ['PATH'].split(':') \
-                    if not p.startswith(('/usr/local','/home','/net','.')) ]
-
-    for p in paths:
-        needle = os.path.join(p, dep)
-        if os.path.exists(needle):
-            return needle
-
-    raise IOError(errno.ENOENT, "Cannot locate binary %s" % dep)
-
-ext_depends_parser = None
-
-def init_ext_depends():
-    global ext_depends_parser
-    if ext_depends_parser:
-        return
-
-    ext_depends_parser = ConfigParser.SafeConfigParser()
-    ext_depends_parser.optionxform = str # we want case-sensitive names
-    ext_depends_parser.read(os.path.expanduser('~/.openerp/modulize-overrides.conf'))
-
-def get_extern_depends(deps):
-    global ext_depends_parser
-    ret = []
-    to_find = []
-    init_ext_depends()
-    if 'python' in deps:
-        for dep in deps['python']:
-            if ext_depends_parser and ext_depends_parser.has_option('python', dep):
-                ret.append('Requires: %s' % ext_depends_parser.get('python', dep))
-                continue
-            try:
-                res = imp.find_module(dep)
-            except ImportError:
-                raise ValueError("This system does not provide the %s python module" % dep)
-            if not res:
-                continue
-            if res[1].startswith(('/usr/local','/home','/net','.')):
-                raise ValueError("Required python package '%s' is at %s" % \
-                                (dep, res[1]))
-
-            to_find.append(res[1])
-    if 'bin' in deps:
-        for dep in deps['bin']:
-            if ext_depends_parser and ext_depends_parser.has_option('bin', dep):
-                ret.append('Requires: %s' % ext_depends_parser.get('bin', dep))
-                continue
-            try:
-                to_find.append(which(dep))
-            except EnvironmentError:
-                raise ValueError("This system does not have the %s binary" % dep)
-
-    for tf in to_find:
-        try:
-            res = subprocess.check_output(['rpm', '-q', \
-                        '--queryformat=Requires: %{NAME}\\n',
-                        '-f', tf], shell=False)
-            ret.append(res.strip())
-        except Exception, e:
-            raise ValueError("Cannot locate dependency for %s: %s" %(tf, e))
-
-    return ('\n'.join(ret) ) + '\n'
-
 def fmt_spec(name, info, allnames, ext_deps=False):
     """ Format the info object fields into a SPEC submodule section
         allnames is a list with all supplied names
@@ -322,7 +202,7 @@ Summary: %s
 Requires: openerp-server >= %s
 """ % (info['name'], rel.version.rsplit('.', 1)[0])
     if 'depends' in info:
-        nii += get_depends(info['depends'],allnames)
+        nii += get_depends(info['depends'], allnames, oname=oname)
     if ext_deps:
         nii += ext_deps
     if 'author' in info:
@@ -374,7 +254,7 @@ for tdir in args:
     if bdir in exclude_modules:
         no_dirs.append(bdir)
         continue
-    info = get_module_info(tdir)
+    info = get_module_info(tdir, rel=rel)
     if (not info ) or (not info.get('installable',True)) :
         no_dirs.append(bdir)
     else :
--- mandriva/python-requires.py
+++ mandriva/python-requires.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
-
 # -*- encoding: utf-8 -*-
 
 #
@@ -35,12 +34,10 @@
 import sys
 import os
 
-import subprocess
-import ConfigParser
-import imp
-import errno
-
 from optparse import OptionParser
+
+sys.path.insert(0, '.')
+from modulize_utils import get_module_info, get_ext_depends, get_extern_depends
 
 parser = OptionParser()
 parser.add_option("-q", "--quiet",
@@ -49,103 +46,8 @@ parser.add_option("-q", "--quiet",
 (options, args) = parser.parse_args()
 
 
-def get_module_info(tname):
-    try:
-        f = open(tname,"r")
-        data = f.read()
-        info = eval(data)
-        #if 'version' in info:
-        #   info['version'] = rel.version.rsplit('.', 1)[0] + '.' + info['version']+rel.subver
-        f.close()
-    except IOError:
-        sys.stderr.write("Dir at %s may not be an OpenERP module.\n" % tname)
-        return {}
-    except:
-        sys.stderr.write(str( sys.exc_info()))
-        return {}
-    return info
 
-def get_ext_depends(deps):
-    ret = []
-    for dep in deps:
-        if type(dep) == type(tuple()):
-            ret.append(dep[0]+' '+ dep[1] + ' ' + dep[2])
-        else:
-            ret.append(dep)
-    ret = set(ret)
-    return ret
-
-def which(dep, paths=[]):
-    """ Locate `dep` among `paths`
-
-        Note: paths will be preserved accross calls
-    """
-
-    if not paths:
-        paths = [ p for p in os.environ['PATH'].split(':') \
-                    if not p.startswith(('/usr/local','/home','/net','.')) ]
-
-    for p in paths:
-        needle = os.path.join(p, dep)
-        if os.path.exists(needle):
-            return needle
-
-    raise IOError(errno.ENOENT, "Cannot locate binary %s" % dep)
-
-ext_depends_parser = None
-
-def init_ext_depends():
-    global ext_depends_parser
-    if ext_depends_parser:
-        return
-
-    ext_depends_parser = ConfigParser.SafeConfigParser()
-    ext_depends_parser.optionxform = str # we want case-sensitive names
-    ext_depends_parser.read(os.path.expanduser('~/.openerp/modulize-overrides.conf'))
-
-def get_extern_depends(deps):
-    global ext_depends_parser
-    ret = []
-    to_find = []
-    init_ext_depends()
-    if 'python' in deps:
-        for dep in deps['python']:
-            if ext_depends_parser and ext_depends_parser.has_option('python', dep):
-                ret.append('Requires: %s' % ext_depends_parser.get('python', dep))
-                continue
-            try:
-                res = imp.find_module(dep)
-            except ImportError:
-                raise ValueError("This system does not provide the %s python module" % dep)
-            if not res:
-                continue
-            if res[1].startswith(('/usr/local','/home','/net','.')):
-                raise ValueError("Required python package '%s' is at %s" % \
-                                (dep, res[1]))
-
-            to_find.append(res[1])
-    if 'bin' in deps:
-        for dep in deps['bin']:
-            if ext_depends_parser and ext_depends_parser.has_option('bin', dep):
-                ret.append('Requires: %s' % ext_depends_parser.get('bin', dep))
-                continue
-            try:
-                to_find.append(which(dep))
-            except EnvironmentError:
-                raise ValueError("This system does not have the %s binary" % dep)
-
-    for tf in to_find:
-        try:
-            res = subprocess.check_output(['rpm', '-q', \
-                        '--queryformat=Requires: %{NAME}\\n',
-                        '-f', tf], shell=False)
-            ret.append(res.strip())
-        except Exception, e:
-            raise ValueError("Cannot locate dependency for %s: %s" %(tf, e))
-
-    return ret
-
-def get_depends(deps):
+def get_odepends(deps):
         ret = []
         for dep in deps:
                 if dep == "base" : continue
@@ -167,7 +69,7 @@ for tfile in args:
         else :
                 openerp_provides.append(os.path.basename(os.path.dirname(tfile)))
                 if 'depends' in info:
-                        openerp_reqs.extend(get_depends(info['depends']))
+                        openerp_reqs.extend(get_odepends(info['depends']))
                 if 'ext_depends' in info:
                         ext_reqs.extend(get_ext_depends(info['ext_depends']))
                 if 'external_dependencies' in info:
